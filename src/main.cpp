5062e51ce59ffc8625dd2826959a155c7561c102
Frank Gao <fgao@lawn-128-61-64-84.lawn.gatech.edu>
2024-12-12 09:35:22 -0500

    clear color

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,18 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (!window) {
@@ -33,6 +39,13 @@ int main() {
     
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     while (!glfwWindowShouldClose(window)) {
+        // check input
+        processInput(window);
+
+        // rendering commands
+        glClearColor(.2f, .3f, .3f, 1.f); // set the color
+        glClear(GL_COLOR_BUFFER_BIT); // color, depth, or stencil buffer
+        
         glfwSwapBuffers(window); // swap back buffer to the front
         glfwPollEvents(); // mouse, keyboard, etc.
     }
